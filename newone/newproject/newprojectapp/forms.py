587af81f648fcbb9875d2bc303cdb3c3e1fa39df
MIDHUN SHAJAN <midhunashajan@gmail.com>
2023-09-21 15:45:45 +0530

    bshhss

---
+++ newone/newproject/newprojectapp/forms.py
@@ -0,0 +1,7 @@
+import form
+from django import forms
+from .models import Movie
+class MovieForm(forms.ModelForm):
+    class Meta:
+        model = Movie
+        fields = ['name','desc','year','img']
